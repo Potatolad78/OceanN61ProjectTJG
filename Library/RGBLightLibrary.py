0ec9afebb0a762b6dfe394ee0baafd5db47847e3
Potatolad78 <rigby3006@gmail.com>
2024-05-20 16:47:10 -0700

    Adding More Abstracting

--- Library/RGBLightLibrary.py
+++ Library/RGBLightLibrary.py
@@ -1,6 +1,8 @@
-from machine import Pin
+from machine import Pin, I2C 
 from time import sleep
 from picozero import RGBLED
+from servo import Servo
+import tsl2561
 #Created by Drew Rigby for group The Melting Pot
 #Version 1.0 4/24/24
 #Feel free to use this API for whatever you need just make sure
@@ -108,5 +110,113 @@ class RGBLight():
         
         #finishes off by turning off the blue led
         self.turnOffBlue()
+    def takeMeassurments(self,color, sleepTime, sensor,loop):
+        f = open("results.txt", "a")
+        for i in range(loop):
+            f.write("Test" + str(i + 1) + "\n")
+            average = 0
+            totalAverage = 0
+            if(color == "red"):
+                f.write("RED: ")
+                self.turnOnRed(255)
+                sensor.read()
+                sleep(sleepTime)
+                for j in range(3):
+                    for i in range(5):
+                        #print(sensor.read())
+                        average += sensor.read()
+                        #sleep(1)
+                    average = average/5
+                    totalAverage += average
+                    f.write("Average" + str(j + 1)+ ": " + str(average)+ " ")
+                    average = 0
+                self.turnOffRed();
+                f.write("Total Average:" + str(totalAverage / 3) + " Sleep Time: " + str(sleepTime)+ " \n")
+                f.write("\n")
+            elif(color == "green"):
+                sleep(sleepTime)
+                f.write("GREEN: ")
+                self.turnOnGreen(255)
+                sensor.read()
+                sleep(sleepTime)
+                for j in range(3):
+                    for i in range(5):
+                        #print(sensor.read())
+                        average += sensor.read()
+                        #sleep(1)
+                    average = average/5
+                    totalAverage += average
+                    f.write("Average" + str(j + 1)+ ": " + str(average)+ " ")
+                    average = 0
+                self.turnOffGreen();
+                f.write("Total Average:" + str(totalAverage / 3) + " Sleep Time: " +  str(sleepTime)+" \n")
+                f.write("\n")
+                
+            elif(color == "blue"):
+                f.write("BLUE: ")
+                self.turnOnBlue(255)
+                sensor.read()
+                sleep(sleepTime)
+                for j in range(3):
+                    for i in range(5):
+                        #print(sensor.read())
+                        average += sensor.read()
+                        #sleep(1)
+                    average = average/5
+                    totalAverage += average
+                    f.write("Average" + str(j + 1)+ ": " + str(average)+ " ")
+                    average = 0
+                self.turnOffBlue();
+                f.write("Total Average:" + str(totalAverage / 3) + " Sleep Time: " +  str(sleepTime)+" \n")
+                f.write("\n")
+            elif(color == "purple"):
+                sleep(sleepTime)
+                f.write("PURPLE: ")
+                self.turnOnPurple()
+                sensor.read()
+                sleep(sleepTime)
+                for j in range(3):
+                    for i in range(5):
+                        #print(sensor.read())
+                        average += sensor.read()
+                        #sleep(1)
+                    average = average/5
+                    totalAverage += average
+                    f.write("Average" + str(j + 1)+ ": " + str(average)+ " ")
+                    average = 0
+                self.turnOffPurple();
+                f.write("Total Average:" + str(totalAverage / 3) + " Sleep Time: " +  str(sleepTime)+ " \n")
+                f.write("\n")
+        f.close()
+class servoMove:
+    def __init__(self, servoPinNum):
+        servoPin = Pin(servoPinNum, Pin.OUT)
+        self.servo = Servo(servoPin)
+        startAngle = 100;
+        endAngle = -30
+        print(servoPin)
     
-
+    def move(self):
+        self.servo.write(100)
+        sleep(1)
+        self.servo.write(-30)
+        sleep(1)
+        self.servo.write(100)
+        
+class powerRelay:
+    def __init__(self, pinNum):
+        self.relay = (pinNum, Pin.OUT)
+        
+    def enableRelay(self):
+        self.relay.value(1)
+    
+    def disableRelay(self):
+        self.relay.value(0)
+        
+    
+class lightSensor:
+    def __init__(self,outPin,sclPin,sdaPin):
+        i2c= I2C(outPin, scl = Pin(sclPin), sda = Pin(sdaPin))
+        self.sensor = tsl2561.TSL2561(i2c)
+    def returnSensor(self):
+        return self.sensor
